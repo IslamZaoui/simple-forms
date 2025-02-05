798e8c484d2e456937de43af1d2945bfa5e36ad5
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2025-02-06 00:21:04 +0100

    add mode watcher to fix theme issue with flash message sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import '../app.css'
+	import { ModeWatcher } from 'mode-watcher'
 	import { FlashMessageProvider } from '@/components/providers'
 
 	let { children } = $props()
 </script>
 
+<ModeWatcher />
 <FlashMessageProvider />
 
 {@render children()}
